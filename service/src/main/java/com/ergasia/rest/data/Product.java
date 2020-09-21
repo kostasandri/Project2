3b915701dcd29e1d7cfcb026d7c02f6cbb121c5e
Konstantinos <Konstantinos@Konstantinos.mshome.net>
2020-09-22 01:03:05 +0300

    r

--- service/src/main/java/com/ergasia/rest/data/Product.java
+++ service/src/main/java/com/ergasia/rest/data/Product.java
@@ -7,25 +7,29 @@ public class Product {
 
 	private int itemID;
 	private String colour;
-	
-	public Product(int itemID, String colour) {
-		this.itemID=itemID;
-		this.colour=colour;
+
+	public Product() {
 	}
-	
+
+	public Product(int itemID, String colour) {
+		this.itemID = itemID;
+		this.colour = colour;
+	}
+
 	public int getItemID() {
 		return itemID;
 	}
+
 	public void setItemID(int itemID) {
 		this.itemID = itemID;
 	}
-	
+
 	public String getColour() {
 		return colour;
 	}
+
 	public void setColour(String colour) {
 		this.colour = colour;
 	}
-	
-	
+
 }
