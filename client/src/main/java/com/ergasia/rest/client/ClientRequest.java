9b7fde56ac1e8f37ccd21aff15c3f2878a80d99b
Konstantinos <Konstantinos@Konstantinos.mshome.net>
2020-09-23 11:17:40 +0300

    c

--- client/src/main/java/com/ergasia/rest/client/ClientRequest.java
+++ client/src/main/java/com/ergasia/rest/client/ClientRequest.java
@@ -1,4 +1,4 @@
- package com.ergasia.rest.client;
+package com.ergasia.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -17,14 +17,15 @@ public class ClientRequest {
 	private int port;
 	private String uri;
 	private final Client client;
-	private HttpAuthenticationFeature feature; 
+	// private HttpAuthenticationFeature feature;
 
 	public ClientRequest() {
 		this.ip = PropertyReader.getIp();
 		this.port = Integer.parseInt(PropertyReader.getPort());
 		uri = "http://" + ip + ":" + port + "/service/webapi/resource/";
 		client = ClientBuilder.newClient();
-		 feature = HttpAuthenticationFeature.basicBuilder().credentials("lazaros", "sarafidis").build();
+		// feature = HttpAuthenticationFeature.basicBuilder().credentials("lazaros",
+		// "sarafidis").build();
 	}
 
 	public ClientRequest(String ip, int port) {
@@ -35,16 +36,16 @@ public class ClientRequest {
 		HttpAuthenticationFeature.basicBuilder().credentials("lazaros", "sarafidis").build();
 	}
 
-	public void createCard() {
-		
-		client.register(feature); // assigns credentials to client
-		
-		WebTarget target = client.target(uri+"createCard");
-		
+	public void createCard(int cID) {
+
+		// client.register(feature); // assigns credentials to client
+
+		WebTarget target = client.target(uri + "client/" + cID + "/createCard");
+
 		Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN_TYPE);
 		Response response = builder.get();
-		//Response response = target.request().get();		
-		
+		// Response response = target.request().get();
+
 		System.out.println(response.readEntity(String.class));
 		System.out.println(response);
 
@@ -55,36 +56,82 @@ public class ClientRequest {
 		 * System.out.println(response.readEntity(String.class));
 		 */
 	}
-	
-	private void updateCard() {
-		client.register(feature); // assigns credentials to client
-		
-		WebTarget target = client.target(uri+"updateCard");
-		
+
+	private void updateCard(int oID, int iID, String colour, int quantity) {			// orderID, itemID
+		// client.register(feature); // assigns credentials to client
+
+		WebTarget target = client.target(uri + "updateCard/{oID}/item/{iID}?colour={colour}&quantity={quantity}");
+
 		Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN_TYPE);
-		Response response = builder.get();	
+		Response response = builder.get();
+
+		System.out.println(response.readEntity(String.class));
+		System.out.println(response);
+
+	}
+
+
+	private void readCard(int oID) {
+		WebTarget target = client.target(uri + "getCard/" + oID);
+
+		Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN_TYPE);
+		Response response = builder.get();
+
+		System.out.println(response.readEntity(String.class));
+		System.out.println(response);
+
+	}
+	
+	private void resetCard(int oID) {
+		WebTarget target = client.target(uri + "resetCard/" + oID);
+
+		Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN_TYPE);
+		Response response = builder.get();
+
+		System.out.println(response.readEntity(String.class));
+		System.out.println(response);
 		
+	}
+	
+	private void history() {
+		WebTarget target = client.target(uri + "history");
+
+		Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN_TYPE);
+		Response response = builder.get();
+
 		System.out.println(response.readEntity(String.class));
 		System.out.println(response);
 		
 	}
 
-	private void deleteCard() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void readCard() {
-		// TODO Auto-generated method stub
-		
-	}
-
 	public static void main(String[] args) {
 		ClientRequest cr = new ClientRequest();
-		cr.createCard();
-		//cr.readCard();
-		//cr.deleteCard();
-		cr.updateCard();
+		for (int i = 0; i < 10; i++) {
+			cr.createCard(i + 5); // iparxoun idi 5 pelates sti vasi
+		}
+		cr.readCard(8);
+		cr.updateCard(8,2,"mauro", 3);
+		cr.updateCard(8,2,"aspro", 2);
+		cr.readCard(8);
+		
+		cr.readCard(9);
+		cr.updateCard(9,1,"prasino",4);
+		cr.readCard(9);
+		cr.updateCard(9,1,"prasino",3);
+		cr.readCard(9);
+		cr.updateCard(9,1,"prasino",0);
+		cr.readCard(9);
+		
+		cr.readCard(10);
+		cr.updateCard(10,1,"prasino",1);
+		cr.updateCard(10,1,"kokkino",1);
+		cr.readCard(9);
+		cr.resetCard(9);
+		cr.readCard(9);
+		
+		cr.history();
+		
+		
 
 	}
 }
